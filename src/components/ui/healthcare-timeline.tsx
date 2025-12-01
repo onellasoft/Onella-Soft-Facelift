2e47c64d93b1c7a3aadad93486c567b55793b0bb
Onella Soft <onellabatchq22025@gmail.com>
2025-12-01 09:51:24 +0000

    decrease  the gap between Comprehensive solutions tailored for the Healt

--- src/components/ui/healthcare-timeline.tsx
+++ src/components/ui/healthcare-timeline.tsx
@@ -160,7 +160,7 @@ export function HealthcareTimeline() {
 
   return (
     <div className="w-full">
-      <div className="max-w-7xl mx-auto py-20 px-4 md:px-8 lg:px-10">
+      <div className="max-w-7xl mx-auto py-10 px-4 md:px-8 lg:px-10">
         <h2 className="text-lg md:text-4xl mb-4 text-white max-w-4xl">
           Comprehensive solutions tailored for the Healthcare Industry
         </h2>
