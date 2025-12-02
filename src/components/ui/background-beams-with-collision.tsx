271942063ddec5a58edfdaa158a8a007a67c3969
Onella Soft <onellabatchq22025@gmail.com>
2025-12-02 12:56:46 +0000

    still there is much white space in it tell me have you applied any class

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "h-96 md:h-[40rem] bg-gradient-to-b from-neutral-900 to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
+        "bg-gradient-to-b from-neutral-900 to-neutral-800 relative flex items-center w-full justify-center overflow-hidden",
         className
       )}
     >
