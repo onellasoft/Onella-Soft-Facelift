e6fce8ae2d2d5098129921f9ac12fc18a97b6345
Onella Soft <onellabatchq22025@gmail.com>
2025-12-02 12:23:09 +0000

    in hospitality page remove our process button from hero section

--- src/components/onella/industries/hospitality/HospitalityHero.tsx
+++ src/components/onella/industries/hospitality/HospitalityHero.tsx
@@ -45,13 +45,6 @@ export function HospitalityHero() {
             >
               Get Started
             </MovingBorderButton>
-            <MovingBorderButton
-              containerClassName="h-11 w-auto"
-              className="bg-transparent text-white px-8"
-              borderRadius="0.5rem"
-            >
-              Our Process
-            </MovingBorderButton>
           </div>
         </div>
       </div>
