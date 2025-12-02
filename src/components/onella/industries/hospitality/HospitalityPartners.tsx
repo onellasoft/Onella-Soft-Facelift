67cb1e664d3c48257032865f5efa31127d39a1d7
Onella Soft <onellabatchq22025@gmail.com>
2025-12-02 12:12:46 +0000

    decrease the gap between hero section and images in hospitality page

--- src/components/onella/industries/hospitality/HospitalityPartners.tsx
+++ src/components/onella/industries/hospitality/HospitalityPartners.tsx
@@ -2,7 +2,7 @@ import { PhotoGallery } from '@/components/ui/photo-gallery';
 
 export function HospitalityPartners() {
   return (
-    <section className="bg-background py-16">
+    <section className="bg-background py-0 md:py-8">
        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <PhotoGallery />
       </div>
