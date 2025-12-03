07cee90e39ef6d18a8c6af96bc41cc9176a7d04d
Onella Soft <onellabatchq22025@gmail.com>
2025-12-03 10:16:07 +0000

    remove all the content from home page only keep footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
-import { Hero } from '@/components/onella/Hero';
-import { WhoWeAre } from '@/components/onella/WhoWeAre';
-import { WhatWeDo } from '@/components/onella/WhatWeDo';
-import { WorkProcess } from '@/components/onella/WorkProcess';
+
 import { Footer } from '@/components/onella/Footer';
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background pt-24">
+    <div className="flex flex-col min-h-screen bg-background">
       <main className="flex-grow">
-        <Hero />
-        <div className="container mx-auto px-4 sm:px-6 lg:px-8 space-y-16 md:space-y-24 py-16 md:py-24">
-          <WhoWeAre />
-          <WorkProcess />
-          <WhatWeDo />
-        </div>
+        {/* Content removed as requested */}
       </main>
       <Footer />
     </div>
