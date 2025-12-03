dcce4f9955e5c1817e751bfbcd15fb2d80563276
Onella Soft <onellabatchq22025@gmail.com>
2025-12-03 09:01:36 +0000

    decrease the gap between hero section and images section

--- src/components/onella/industries/ecommerce/EcommerceHero.tsx
+++ src/components/onella/industries/ecommerce/EcommerceHero.tsx
@@ -47,7 +47,7 @@ export function EcommerceHero() {
           </MovingBorderButton>
         </div>
       </section>
-      <section className="bg-background py-0 md:py-8">
+      <section className="bg-background">
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
             <PhotoGallery imageSet="ecommerce" />
         </div>
